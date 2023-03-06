1bfd1439074cd01cdc6a1fdbbb95c377fe960213
Carlos Lima <tec.carlos.lima@gmail.com>
2023-03-05 22:18:25 -0300

    adcionado campo user, retornando name e funcao

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,22 +6,27 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 USE Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function login(Request $request){
         if(Auth::attempt(['email' => $request -> email, 'password' => $request -> password])){
             $user = Auth::user();
+            $isToken = $user->tokens;
+            if($isToken != null){
+                $user->tokens()->delete();
+            }
             $token = $user->createToken('JWT');
 
-            return response()->json($token->plainTextToken, 200);
+            return response()->json(['token' => $token->plainTextToken, 'user' => ['name' => $user->name, 'function' => $user->function]], 200);
         }
         return response()->json('Usuario invalido', 401);
     }
 
     public function store(Request $request, User $user)
     {
-        $userData = $request->only('name', 'email', 'password');
+        $userData = $request->only('name', 'email', 'password', 'function');
         $userData['password'] = Hash::make($userData['password']);
         if(!$user = $user->create($userData)){
             abort(500, "Erro");
