2ebe26a16e96dc348b3bab028e498bec1edd8ed6
Carlos Lima <tec.carlos.lima@gmail.com>
2023-03-18 05:57:18 -0300

    seed padrao para base

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,5 +23,44 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DB::table('users')->insert([
+            [
+                'name' => 'Validação',
+                'function' => 'Validação',
+                'email' => 'validacao@medeiros.api',
+                'password' => Hash::make('360632'),
+            ],
+            [
+                'name' => 'Carlos Lima',
+                'function' => 'Adim',
+                'email' => 'carlos@medeiros.api',
+                'password' => Hash::make('3012api'),
+            ],
+            [
+                'name' => 'Repositor',
+                'function' => 'Repositor',
+                'email' => 'repositor@medeiros.api',
+                'password' => Hash::make('360632'),
+            ],
+            [
+                'name' => 'Promotor',
+                'function' => 'promotor',
+                'email' => 'promotor@medeiros.api',
+                'password' => Hash::make('360632'),
+            ],
+            [
+                'name' => 'Brigada de validade',
+                'function' => 'brigada',
+                'email' => 'brigada@medeiros.api',
+                'password' => Hash::make('360632'),
+            ],
+            [
+                'name' => 'Controladoria',
+                'function' => 'controladoria',
+                'email' => 'controladoria@medeiros.api',
+                'password' => Hash::make('360632'),
+            ]
+        ]);
     }
 }
