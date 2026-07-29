cceef67619debdbd1e5172d8d187be5cfd857dac
Carlos Lima <tec.carlos.lima@gmail.com>
2026-07-28 23:11:35 -0300

    Bump version to 2.1.2

--- config/app.php
+++ config/app.php
@@ -17,7 +17,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
-    'version' => env('APP_VERSION', '1.0.0'),
+    'version' => env('APP_VERSION', '2.1.2'),
 
     /*
     |--------------------------------------------------------------------------
